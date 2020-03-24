d59a90f086a07f88250c11bee637e263fd76de1b
minh.phan81299 <minh.phan81299@gmail.com>
2020-03-24 10:53:48 +0700

    say hi method

--- Dog.js
+++ Dog.js
@@ -1,8 +1,19 @@
-function function_name(argument) {
+var chalk =require('chalk');
+
+function Dog(name) {
 	// body...
+	this.name=name;
 	this.stomach=[];
 }
 Dog.prototype.eat = function(cat) {
 	// body...3
 	this.stomach.push(cat);
 };
+
+Dog.prototype.sayhi = function() {
+	// body...3
+	console.log('hi i am a dog'+ chalk.red(this.name));
+
+};
+
+module.exports = Dog;
